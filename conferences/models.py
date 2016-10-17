7f07de4026b704cc82b66fb3ce53ff395b01a9ec
scoiatael <czapl.luk@gmail.com>
2016-10-18 00:48:37 +0200

    Extract place to separate model

--- conferences/models.py
+++ conferences/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 
+class Place(models.Model):
+    place_name = models.CharField(
+        max_length=300
+    )
+    place_url = models.CharField(
+        max_length=300
+    )
+    place_address = models.TextField()
+
+    def __str__(self):
+        return self.place_name
+
+
 class Zosia(models.Model):
     start_date = models.DateField(
         verbose_name=_('First day')
@@ -14,17 +27,11 @@ class Zosia(models.Model):
         default=False
     )
 
-    place_name = models.CharField(
-        max_length=300
-    )
-    place_url = models.CharField(
-        max_length=300
-    )
-    place_address = models.TextField()
-
     banner = models.ImageField()
 
+    place = models.ForeignKey(Place)
+
     def __str__(self):
-        return 'In {} from {} to {}'.format(str(self.place_name),
+        return 'In {} from {} to {}'.format(str(self.place),
                                             self.start_date,
                                             self.end_date)
