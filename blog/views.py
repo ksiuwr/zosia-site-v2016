fc6c6de60af5909f8743de748bcbd9c0b9fa8532
Mira <szewczyk.mira@gmail.com>
2016-11-03 19:22:28 +0100

    First version of views in blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
+from models import BlogPost
+from forms import BlogPostForm
 
-# Create your views here.
+
+def index(request):
+    ctx = {'objects': BlogPost.objects.all()}
+    return render(request, 'blog/index.html', ctx)
+
+@staff_member_required
+def create(request):
+    ctx = {'form': BlogPostForm(request.POST or None)}
+
+    if request.method == 'POST':
+        if ctx['form'].is_valid():
+            ctx['form'].save()
+    return render(request, 'blog.create.html', ctx)
