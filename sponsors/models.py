51a0543f6f333fc44f1c58b3e0bb46668a7f6465
Mira <szewczyk.mira@gmail.com>
2016-11-09 19:05:45 +0100

    Sponsors model and migrations created

--- sponsors/models.py
+++ sponsors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Sponsor(models.Model):
+    name = models.CharField(verbose_name=_("Name"), max_length=200, unique=True)
+    is_active = models.BooleanField(verbose_name=_("Active"), default=False)
+    url = models.URLField(verbose_name=_("URL"), max_length=200, blank=True,
+                          null=True)
+    logo = models.ImageField(verbose_name=_("Logo"), upload_to='sponsors')
+
+    def __str__(self):
+        return self.name
