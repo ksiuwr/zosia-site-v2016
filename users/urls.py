09186b35ea6ee6ca9074091277f88ffed9d85bb7
scoiatael <czapl.luk@gmail.com>
2016-11-13 13:03:26 +0100

    Login page

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,13 @@ from . import views
 
 urlpatterns = [
     url('^profile/$', views.profile),
+    # ^login/$',
+    # ^logout/$',
+    # ^password_change/$',
+    # ^password_change/done/$',
+    # ^password_reset/$',
+    # ^password_reset/done/$',
+    # ^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    # ^reset/done/$',
     url('^', include('django.contrib.auth.urls')),
 ]
