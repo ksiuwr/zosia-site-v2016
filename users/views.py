fedfc1f4a59c37816bfa6c5808d45aad114f5117
scoiatael <czapl.luk@gmail.com>
2016-11-13 14:11:00 +0100

    Fix pep, remove obsolete user context

--- users/views.py
+++ users/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user
 
 
 # Create your views here.
 @login_required
 def profile(request):
-    ctx = {
-        'user' : get_user(request)
-    }
-    return render(request, 'users/profile.html', ctx)
+    return render(request, 'users/profile.html')
