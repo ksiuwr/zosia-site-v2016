5fdb56ad8943b8ef61c9bb51ef2b636de58771c6
scoiatael <czapl.luk@gmail.com>
2016-11-13 13:37:57 +0100

    Password change, profile

--- users/urls.py
+++ users/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url('^profile/$', views.profile),
-    # ^login/$',
-    # ^logout/$',
-    # ^password_change/$',
-    # ^password_change/done/$',
-    # ^password_reset/$',
-    # ^password_reset/done/$',
-    # ^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    # ^reset/done/$',
+    url('^profile/$', views.profile, name='accounts_profile'),
+    # ^login/$ [name='login']
+    # ^logout/$ [name='logout']
+    # ^password_change/$ [name='password_change']
+    # ^password_change/done/$ [name='password_change_done']
+    # ^password_reset/$ [name='password_reset']
+    # ^password_reset/done/$ [name='password_reset_done']
+    # ^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$ [name='password_reset_confirm']
+    # ^reset/done/$ [name='password_reset_complete']
     url('^', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user
 
 
 # Create your views here.
 @login_required
 def profile(request):
-    return HttpResponse('Hello, {}'.format(str(request.user)))
+    ctx = {
+        'user' : get_user(request)
+    }
+    return render(request, 'users/profile.html', ctx)
