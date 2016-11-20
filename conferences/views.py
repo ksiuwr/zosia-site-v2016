3cf75cb4cb6523edfd472f9fab6cb1b7cceffc7e
scoiatael <czapl.luk@gmail.com>
2016-11-20 21:30:21 +0100

    Add register view, some trans

--- conferences/views.py
+++ conferences/views.py
@@ -1,8 +1,10 @@
 from urllib.parse import urlencode
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
+
 from .models import Zosia
 
 
@@ -21,3 +23,11 @@ def index(request):
         }
         context['gapi_place_src'] = GAPI_PLACE_BASE_URL + '?' + urlencode(query)
     return render(request, 'conferences/index.html', context)
+
+@login_required
+def register(request, zosia_id):
+    zosia = get_object_or_404(Zosia, pk=zosia_id)
+    context = {
+        'zosia': zosia,
+    }
+    return render(request, 'conferences/register.html', context)
