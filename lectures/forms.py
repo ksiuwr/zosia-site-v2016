842b00431bd5d29ce1da1fd95b0103e8c8d1b793
Mira <szewczyk.mira@gmail.com>
2016-11-28 22:04:42 +0100

    Views mock up with forms

---
+++ lectures/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Lecture
+
+
+class LectureForm(forms.ModelForm):
+
+    class Meta:
+        model = Lecture
+        fields = ['info', 'title', 'abstract', 'duration', 'lecture_type',
+                  'person_type']
+
+
+class LectureAdminForm(forms.ModelForm):
+    class Meta:
+        model = Lecture
+        exclude = ['zosia', 'create_date']
--- lectures/views.py
+++ lectures/views.py
@@ -1,3 +1,77 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.utils.translation import ugettext_lazy as _
+from conferences.models import Zosia
+from .forms import LectureForm, LectureAdminForm
 
-# Create your views here.
+
+@require_http_methods(['GET'])
+def index(request):
+    """
+    Display all accepted lectures
+    """
+    zosia = Zosia.objects.find_active()
+    lectures = Lecture.objects.select_related('author').filter(
+                zosia=zosia).filter(active=True)
+    ctx = {'objects': lectures}
+    # TODO: add return
+    # TODO: create template
+
+
+@staff_member_required()
+def display_all_staff(request):
+    """
+    Display all for staff members, they can change acceptation status
+    """
+    zosia = Zosia.objects.find_active()
+    lectures = Lecture.objects.select_related('author').filter(zosia=zosia)
+    # TODO: create return, add template
+
+
+@staff_member_required()
+@require_http_methods(['POST'])
+def toggle_accept(request):
+    """
+    ajax for accepting lecture
+    """
+    # TODO: create
+    pass
+
+
+@login_required()
+def lecture_add(request):
+    """
+    participant can add his own lecture
+    """
+    # TODO: review view
+    zosia = Zosia.objects.find_active()
+    ctx = {'form': LectureForm(request.POST or None)}
+    if request.method == 'POST':
+        if ctx['form'].is_valid():
+            lecture = ctx['form'].save(commit=False)
+            lecture.zosia = zosia
+            lecture.author = request.user
+            messages.succes(request, _("Lecture has been saved"))
+            # TODO: add to url file
+            return redirect('lectures_index')
+        else:
+            messages.error(request, _("Please review your form"))
+    # TODO: create template
+    return render(request, 'lectures/add.html', ctx)
+
+
+@staff_member_required()
+def lecture_update(request):
+    """
+    Staff member can add lecture for other user, can edit all lectures
+    """
+    # TODO: create
+    pass
+
+
+def schedule_display(request):
+    # TODO: create
+    pass
