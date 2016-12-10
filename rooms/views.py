4c8f1a2c925fe5258676e10cf366c89a8b505362
scoiatael <czapl.luk@gmail.com>
2016-12-11 00:35:33 +0100

    Views scaffold

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# GET
+# Cache
+def rooming(request):
+    # Return HTML w/ rooms layout
+    pass
+
+
+# GET
+# Don't cache
+def rooms(request):
+    # Ajax
+    # Return JSON view of rooms
+    pass
+
+
+# POST
+def join(request):
+    # Ajax
+    # Try to join given room
+    pass
