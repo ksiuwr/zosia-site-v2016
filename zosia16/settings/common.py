942a36de26d41535468b70db193b658ab02aed6c
Mira <szewczyk.mira@gmail.com>
2017-02-05 19:04:38 +0000

    Add context processor with zosia

--- zosia16/settings/common.py
+++ zosia16/settings/common.py
@@ -99,6 +99,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'conferences.context_processors.get_zosia',
             ],
         },
     },
