6ef168a1687c78192690e93eec0e8de55d5e98d8
Mira <szewczyk.mira@gmail.com>
2017-02-05 19:24:00 +0000

    Remove context processor

--- zosia16/settings/common.py
+++ zosia16/settings/common.py
@@ -99,7 +99,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'conferences.context_processors.get_zosia',
             ],
         },
     },
