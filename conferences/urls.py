d49c339c66cef4dfbfa60ae6c15570c5af4fe579
Mira <szewczyk.mira@gmail.com>
2017-02-11 13:47:14 +0000

    Add admin panel homepage

--- conferences/urls.py
+++ conferences/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<zosia_id>\d+)/register$', views.register, name='user_zosia_register'),
     url(r'^terms/$', views.terms_and_conditions, name='terms_and_conditions'),
+    url(r'^panel/$', views.admin_panel, name='admin'),
 ]
