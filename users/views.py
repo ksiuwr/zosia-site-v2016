595671e5bc38b49e9b2554070a2b32a5752e37a5
Mira <szewczyk.mira@gmail.com>
2017-04-21 22:21:44 +0200

    Display user preferences

--- users/views.py
+++ users/views.py
@@ -16,7 +16,8 @@ def profile(request):
     ctx = {}
     current_zosia = Zosia.objects.find_active()
     if current_zosia:
-        current_prefs = UserPreferences.objects.filter(zosia=current_zosia, user=request.user).first()
+        current_prefs = UserPreferences.objects.select_related('bus').filter(
+            zosia=current_zosia, user=request.user).first()
         if current_prefs:
             ctx['current_prefs'] = current_prefs
 
