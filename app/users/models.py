3c9cc47f86a3c45d37407c215486c1e3a41dd152
Mateusz Urbańczyk <mu@qed.ai>
2019-01-14 13:04:01 +0100

    User by default should NOT be admin!

--- app/users/models.py
+++ app/users/models.py
@@ -26,7 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
-    is_staff = models.BooleanField(_('staff'), default=True)
+    is_staff = models.BooleanField(_('staff'), default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
