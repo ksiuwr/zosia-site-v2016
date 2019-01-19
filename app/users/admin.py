65ff897c944a691c7ed864057a8d59d00210d1e0
Piotr Szymajda <PiotrSzymajda@10g.pl>
2019-01-19 16:27:04 +0100

    Add reject action for organizations

--- app/users/admin.py
+++ app/users/admin.py
@@ -3,14 +3,23 @@ from .models import Organization, User
 
 admin.site.register(User)
 
+
 def accept_organization(modeladmin, request, queryset):
     for organ in queryset:
         organ.accepted = True
         organ.save()
-        
+
 accept_organization.short_description = 'Accept selected organizations'
 
+def reject_organization(modeladmin, request, queryset):
+    for organ in queryset:
+        organ.accepted = False
+        organ.save()
+        
+accept_organization.short_description = 'Reject selected organizations'
+
+
 class OrganizationAdmin(admin.ModelAdmin):
-    actions = [accept_organization, ]
+    actions = [accept_organization, reject_organization, ]
 
 admin.site.register(Organization, OrganizationAdmin)
