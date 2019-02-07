e868a21b3deb816fcf147adb8a2d280f0862abef
Piotr Szymajda <PiotrSzymajda@10g.pl>
2019-02-07 12:09:24 +0100

    Change convert_bonus_to_time to propery

--- app/conferences/models.py
+++ app/conferences/models.py
@@ -262,6 +262,7 @@ class UserPreferences(models.Model):
     def room(self):
         return self.user.room_set.for_zosia(self.zosia).first()
 
+    @property
     def convert_bonus_to_time(self):
         opening_time = datetime.combine(self.zosia.rooming_start, datetime.min.time())
         return opening_time - timedelta(0, 3*60*self.bonus_minutes)
