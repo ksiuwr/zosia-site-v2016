2c83deee40633cfa6422dc38acbf26db1e9b9148
Jakub Szczerbiński <vszczerbinski@gmail.com>
2019-06-30 15:15:52 +0200

    Updating django to 2.2 and fixing models

--- app/users/utils.py
+++ app/users/utils.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 def anonymous_required(view):
     @wraps(view)
     def func(request, *args, **kwargs):
-        if request.user.is_authenticated():
+        if request.user.is_authenticated:
             return redirect('index')
         return view(request, *args, **kwargs)
 
