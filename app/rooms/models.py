7246e82613a554247bba1ae1f37b4a0df8cc9999
R. Kaleta <rafkaleta@onet.pl>
2019-07-04 23:07:31 +0200

    Added RoomBeds model, formatting code

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -2,25 +2,26 @@ import random
 import string
 from datetime import timedelta
 
-from django.utils import timezone
-from django.db import models, transaction
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
-
-from users.models import User
 from conferences.models import Zosia
+from django.core.exceptions import ValidationError
+from django.db import models, transaction
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from users.models import User
 
 
 def random_string(length=10):
-    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(length))
+    return ''.join(
+        random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(length))
 
 
 class RoomLockManager(models.Manager):
     # 3 hours
-    TIMEOUT = timedelta(0, 3*3600)
+    TIMEOUT = timedelta(0, 3 * 3600)
 
     def make(self, user, expiration=None):
         expiration = expiration or self.TIMEOUT
+
         return self.create(user=user,
                            password=random_string(4),
                            expiration_date=timezone.now() + expiration)
@@ -47,29 +48,23 @@ class RoomLock(models.Model):
 
 class RoomManager(models.Manager):
     def for_zosia(self, zosia, **override):
-        defaults = {
-            'zosia': zosia,
-            'hidden': False,
-        }
+        defaults = {'zosia': zosia, 'hidden': False}
         defaults.update(**override)
+
         return self.filter(**defaults)
 
 
 class Room(models.Model):
     objects = RoomManager()
 
-    name = models.CharField(
-        max_length=300)
-    description = models.TextField(
-        default='')
+    name = models.CharField(max_length=300)
+    description = models.TextField(default='')
     capacity = models.IntegerField()
     hidden = models.BooleanField(default=False)
-    zosia = models.ForeignKey(Zosia, models.CASCADE)
 
-    lock = models.ForeignKey(RoomLock,
-                             on_delete=models.SET_NULL,
-                             blank=True,
-                             null=True)
+    zosia = models.ForeignKey(Zosia, on_delete=models.CASCADE)
+    lock = models.ForeignKey(
+        RoomLock, on_delete=models.SET_NULL, blank=True, null=True)
 
     users = models.ManyToManyField(User, through='UserRoom')
 
@@ -90,32 +85,33 @@ class Room(models.Model):
         if self.is_locked and not self.lock.unlocks(password):
             return ValidationError(_('Cannot join room %(room), is locked.'),
                                    code='invalid',
-                                   params={
-                                       'room': self
-                                   })
+                                   params={'room': self})
 
         # Ensure room is not full
         if self.occupied >= self.capacity:
             return ValidationError(_('Cannot join room %(room), is full.'),
                                    code='invalid',
-                                   params={
-                                       'room': self
-                                   })
+                                   params={'room': self})
 
         # Remove user from previous rooms
-        prev_userroom = user.userroom_set.select_related('room').filter(room__zosia=self.zosia).first()
+        prev_userroom = user.userroom_set.select_related(
+            'room').filter(room__zosia=self.zosia).first()
+
         if prev_userroom:
             if prev_userroom.room.is_locked and prev_userroom.room.lock.owns(user):
                 prev_userroom.room.lock.delete()
+
             prev_userroom.delete()
 
         owner_lock = None
+
         if lock:
             if not self.lock or self.lock.is_expired:
                 owner_lock = RoomLock.objects.make(user, expiration=expiration)
                 self.lock = owner_lock
 
         self.save()
+
         return UserRoom.objects.create(room=self, user=user)
 
     @transaction.atomic
@@ -125,6 +121,18 @@ class Room(models.Model):
 
     def __str__(self):
         return 'Room ' + self.name
+
+
+class RoomBeds(models.Model):
+    single = models.IntegerField(default=0)
+    double = models.IntegerField(default=0)
+    other = models.IntegerField(default=0)
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    @property
+    def capacity(self):
+        return self.single + 2 * self.double + self.other
 
 
 class UserRoom(models.Model):
