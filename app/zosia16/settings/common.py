99596b0ae7e87132ea45a504602a85740ea2a14f
Piotr Szymajda <PiotrSzymajda@10g.pl>
2019-07-18 19:05:23 +0200

    Change hashing algorithm to Argon2 ^^

--- app/zosia16/settings/common.py
+++ app/zosia16/settings/common.py
@@ -143,6 +143,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# List of hashing algorithm classes
+# https://docs.djangoproject.com/en/2.2/topics/auth/passwords/
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
