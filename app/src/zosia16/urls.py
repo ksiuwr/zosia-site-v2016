5e35ad953ba72c3fb331c1d40efc0db23c62bdf8
R. Kaleta <rafkaleta@onet.pl>
2019-07-22 18:55:41 +0200

    Add joined_at column to userroom

--- app/src/zosia16/urls.py
+++ app/src/zosia16/urls.py
@@ -38,4 +38,4 @@ urlpatterns = \
 if settings.DEBUG:
     import debug_toolbar
 
-urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
