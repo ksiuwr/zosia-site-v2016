df923fccd9a7b9748a4bbaa4911bef9a5e81726e
R. Kaleta <rafkaleta@onet.pl>
2019-09-10 18:32:12 +0200

    Tests for users API

---
+++ app/src/users/api/tests.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from conferences.test_helpers import new_user, new_zosia
+
+
+class UsersAPIViewTestCase(APITestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.zosia = new_zosia(active=True)
+
+        self.normal_1 = new_user(0)
+        self.normal_2 = new_user(1)
+        self.staff_1 = new_user(2, is_staff=True)
+        self.staff_2 = new_user(3, is_staff=True)
+
+    def tearDown(self):
+        self.client.force_authenticate(user=None)
+        super().tearDown()
+
+
+class UserListAPIViewTestCase(UsersAPIViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("users_api_list", kwargs={"version": "v1"})
+
+    def test_user_cannot_get_all_users(self):
+        self.client.force_authenticate(user=self.normal_1)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_staff_can_get_all_users(self):
+        self.client.force_authenticate(user=self.staff_2)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 4)
+
+
+class SessionUserAPIViewTestCase(UsersAPIViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("users_api_me", kwargs={"version": "v1"})
+
+    def test_user_can_get_their_info(self):
+        self.client.force_authenticate(user=self.normal_2)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.normal_2.email, response.data["email"])
+        self.assertEqual(self.normal_2.first_name, response.data["first_name"])
+        self.assertEqual(self.normal_2.last_name, response.data["last_name"])
+
+    def test_staff_can_get_their_info(self):
+        self.client.force_authenticate(user=self.staff_1)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.staff_1.email, response.data["email"])
+        self.assertEqual(self.staff_1.first_name, response.data["first_name"])
+        self.assertEqual(self.staff_1.last_name, response.data["last_name"])
+
+    def test_anonymous_cannot_get_their_info(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
