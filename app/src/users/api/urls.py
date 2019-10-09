78792a5642f212a3c189eb84c85db6622b7abf62
R. Kaleta <rafkaleta@onet.pl>
2019-10-09 18:30:18 +0200

    ViewSet action for user from session

--- app/src/users/api/urls.py
+++ app/src/users/api/urls.py
@@ -6,5 +6,5 @@ from users.api import views
 urlpatterns = [
     path("", views.UserViewSet.as_view({"get": "list"}), name="users_api_list"),
     path("<int:pk>/", views.UserViewSet.as_view({"get": "retrieve"}), name="users_api_detail"),
-    path("me/", views.session_user, name="users_api_me")
+    path("me/", views.UserViewSet.as_view({"get": "session"}), name="users_api_me")
 ]
--- app/src/users/api/views.py
+++ app/src/users/api/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -12,12 +12,15 @@ from users.models import User
 class UserViewSet(ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
 
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated] if self.action == "session" else [IsAdminUser]
 
-@api_view(["GET"])
-def session_user(request, version):
-    sender = request.user
-    serializer = UserSerializer(sender, context={'request': request})
+        return [permission() for permission in permission_classes]
 
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    @action(detail=False, methods=["GET"])
+    def session(self, request, version):
+        sender = request.user
+        serializer = self.get_serializer(sender)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
