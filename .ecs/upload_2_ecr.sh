fecd464970cf33c6b6016bf10e339f74a52680ec
Piotr Szymajda <PiotrSzymajda@10g.pl>
2020-01-07 16:37:34 +0100

    Fix bug image uploading script

--- .ecs/upload_2_ecr.sh
+++ .ecs/upload_2_ecr.sh
@@ -7,12 +7,9 @@ Please remember about setting proper repository URL and AWS region.
 
 # Configuration
 DOCKER_REPO_URI_BASE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
-
+ZOSIA_IMG_NAME="zosia_prod_web"
+NGINX_IMG_NAME="zosia_prod_nginx"
 TAG="${VERSION_TAG}"
-ZOSIA_IMG_NAME="zosia_prod_web:${TAG}"
-NGINX_IMG_NAME="zosia_prod_nginx:${TAG}"
-
-echo ${TAG}
 
 # Login and upload images
 DOCKER_LOGIN_CMD=`aws ecr get-login --region ${AWS_DEFAULT_REGION} --no-include-email`
@@ -20,20 +17,15 @@ echo "Logging to remote registry with command"
 eval ${DOCKER_LOGIN_CMD}
 
 # upload zosia_web image
-ZOSIA_IMG_ID=`docker images ${ZOSIA_IMG_NAME} | tail -1 | awk '{ print $3 }'`
+ZOSIA_IMG_ID=`docker images ${ZOSIA_IMG_NAME}:latest | tail -1 | awk '{ print $3 }'`
 
 DOCKER_REPO_URI="${DOCKER_REPO_URI_BASE}/zosia_web"
 REGISTRY_IMG_NAME="${DOCKER_REPO_URI}:${TAG}"
-
-echo ${ZOSIA_IMG_ID}
-echo ${DOCKER_REPO_URI}
-echo ${REGISTRY_IMG_NAME}
-
 docker tag ${ZOSIA_IMG_ID} ${REGISTRY_IMG_NAME}
 docker push ${REGISTRY_IMG_NAME}
 
 # upload zosia_nginx image
-NGINX_IMG_ID=`docker images ${NGINX_IMG_NAME} | tail -1 | awk '{ print $3 }'`
+NGINX_IMG_ID=`docker images ${NGINX_IMG_NAME}:latest | tail -1 | awk '{ print $3 }'`
 
 DOCKER_REPO_URI="${DOCKER_REPO_URI_BASE}/zosia_nginx"
 REGISTRY_IMG_NAME="${DOCKER_REPO_URI}:${TAG}"
