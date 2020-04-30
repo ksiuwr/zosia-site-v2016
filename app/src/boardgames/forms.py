e98e2e850f68d53e1d7b5d697bbcbbf5d11c0761
R. Kaleta <rafkaleta@onet.eu>
2020-04-30 16:28:52 +0200

    Use implicit string concatenation

--- app/src/boardgames/forms.py
+++ app/src/boardgames/forms.py
@@ -4,5 +4,5 @@ from django.utils.safestring import mark_safe
 
 class BoardgameForm(forms.Form):
     url = forms.CharField(
-        label=mark_safe('Find your boardgame on: <a href="https://boardgamegeek.com/">\
-            boardgamegeek.com</a> and paste here link'), max_length=200)
+        label=mark_safe('Find your boardgame on: <a href="https://boardgamegeek.com/">'
+            'boardgamegeek.com</a> and paste the link here'), max_length=200)
