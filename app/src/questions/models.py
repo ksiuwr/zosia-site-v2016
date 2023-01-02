10b34c72f3de7617d1f6190755127294f5a3c390
mpnogaj <mpnogaj@gmail.com>
2023-01-02 13:15:02 +0100

    Added priorities to Q&A

--- app/src/questions/models.py
+++ app/src/questions/models.py
@@ -5,3 +5,5 @@ from django.utils.translation import ugettext_lazy as _
 class QA(models.Model):
     question = models.CharField(_('Question'), max_length=150)
     answer = models.CharField(_('Answer'), max_length=500)
+    # will be sorted in descending order
+    priority = models.PositiveSmallIntegerField(_('Priority'), default=0)
