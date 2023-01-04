c8dcfd98a55362c3ddcc7569d73669bc5950ecd6
mpnogaj <mpnogaj@gmail.com>
2023-01-04 21:54:29 +0100

    Added help text to priority field

--- app/src/questions/models.py
+++ app/src/questions/models.py
@@ -5,5 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 class QA(models.Model):
     question = models.CharField(_('Question'), max_length=150)
     answer = models.CharField(_('Answer'), max_length=500)
-    # will be sorted in descending order
-    priority = models.PositiveSmallIntegerField(_('Priority'), default=0)
+    priority = models.PositiveSmallIntegerField(_('Priority'), default=0, help_text='Questions are sorted in descending order of priorities')
