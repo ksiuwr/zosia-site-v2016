debc77c07777484f5a80d25bd039203e4800d8bf
mpnogaj <mpnogaj@gmail.com>
2023-01-20 20:36:28 +0100

    Added phone number validation

--- app/src/organizers/models.py
+++ app/src/organizers/models.py
@@ -1,8 +1,17 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from users.models import User
 from conferences.models import Zosia
+
+def validate_phone_number(value):
+    phone_reg = r'\d{9}'
+    m = re.match(phone_reg, value)
+    if not m:
+        raise ValidationError(_('Please provide correct phone number'))
 
 class OrganizerContact(models.Model):
     user = models.OneToOneField(
@@ -12,7 +21,11 @@ class OrganizerContact(models.Model):
         verbose_name=_('User'),
         limit_choices_to={'is_staff': True}
     )
-    phone_number = models.CharField(verbose_name=_('Phone number'), max_length=9)
+    phone_number = models.CharField(
+        verbose_name=_('Phone number'), 
+        max_length=9,
+        validators=[validate_phone_number]
+    )
     zosia = models.ForeignKey(
         Zosia,
         on_delete=models.CASCADE,
